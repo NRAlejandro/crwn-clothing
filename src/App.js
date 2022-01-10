4f973b25f4c9c58cf577f0d5ac50ffb0c928da45
NRAlejandro <nicolas.ramirez.alejandro@gmail.com>
2022-01-10 15:33:56 -0300

    Updating app with components: shop page, collection and collection preview. Added respective styles too.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
