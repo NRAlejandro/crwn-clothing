14ae4db14e61e931a5fd9aab96bb499ab9755d2a
NRAlejandro <nicolas.ramirez.alejandro@gmail.com>
2022-02-15 15:36:51 -0300

    Install and then update all components with styled-components, deleted old scss files

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -5,7 +5,7 @@ import CustomButtom from '../custom-button/custom-button.component';
 
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
-import './sign-up.styles.scss';
+import { SignUpContainer, SignUpTitle } from './sign-up.styles'
 
 class SignUp extends React.Component {
     constructor() {
@@ -59,8 +59,8 @@ class SignUp extends React.Component {
 
         const { displayName, email, password, confirmPassword } = this.state;
         return (
-            <div className='sign-up'>
-                <h2 className='title'>I do not have a account</h2>
+            <SignUpContainer>
+                <SignUpTitle>I do not have a account</SignUpTitle>
                 <span>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
                     <FormInput
@@ -97,7 +97,7 @@ class SignUp extends React.Component {
                     />
                     <CustomButtom type='submit'>Sign Up</CustomButtom>
                 </form>
-            </div>
+            </SignUpContainer>
         );
     }
 }
